46b96bbccaf41fbb3515d2d29f0b4df0350cff21
nparsley <nickparsley5@gmail.com>
2021-08-14 18:50:14 -0700

    uri,connection string

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,47 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
-app.get('/', (req, res, next) => {
-    res.send('hello api')
+
+
+// app.use(bodyParser.json()) with app.use(express.json())
+
+// middleware
+app.use(express.json());
+app.use(morgan('tiny'));
+
+require('dotenv/config');
+const api = process.env.API_URL;
+
+// routes
+app.get(`${api}/products`, (req, res, next) => {
+    const product = {
+        id: 1,
+        name: 'hair dresser',
+        image: 'some_url'
+    }
+    res.send(product);
 })
 
+app.post(`${api}/products`, (req, res, next) => {
+    const newProduct = req.body;
+    console.log(newProduct);
+
+    res.send(newProduct);
+})
+
+// uri from mongodb cloud(connection string)
+mongoose.connect(process.env.CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        console.log('database connection is ready...')
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
 app.listen(3000, () => {
+    // console.log(api);
     console.log('server running http://localhost:3000');
 })
 
