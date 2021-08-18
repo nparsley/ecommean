c602391f69cf44c74c70f43049edb745e503a809
nparsley <nickparsley5@gmail.com>
2021-08-18 11:31:08 -0700

    categories update,deatils,get

--- backend/routers/categories.js
+++ backend/routers/categories.js
@@ -8,7 +8,15 @@ router.get(`/`, async (req, res) => {
     if(!categoryList) {
         res.status(500).json({success: false})
     }
-    res.send(categoryList);
+    res.status(200).send(categoryList);
+})
+
+router.get('/:id', async (req, res) => {
+    const category = await Category.findById(req.params.id);
+    if(!category) {
+        res.status(500).json({message: 'the category with the given ID was not found'})
+    }
+    res.status(200).send(category);
 })
 
 router.post('/', async (req, res) => {
@@ -21,6 +29,22 @@ router.post('/', async (req, res) => {
 
     if(!category) {
         return res.status(404).send('the category cannot be created')
+    }
+    res.send(category);
+})
+
+router.put('/:id', async (req, res) => {
+    const category = await Category.findByIdAndUpdate(
+        req.params.id,  
+        {   name: req.body.name,
+            icon: req.body.icon,
+            color: req.body.color
+        },
+        // returns new updated data
+        { new: true }
+    )
+    if(!category) {
+        return res.status(400).send('the category cannot be created')
     }
     res.send(category);
 })
