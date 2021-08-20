3ce53092c3e895cb14af898a6bf192b09cfb0f5b
nparsley <nickparsley5@gmail.com>
2021-08-19 19:58:31 -0700

    post new product + rest api

--- backend/routers/products.js
+++ backend/routers/products.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const { Category } = require('../models/category');
 const router = express.Router();
 const {Product} = require('../models/product');
 
 
-router.get(`/`, async (req, res, next) => {
-// router.get(`${api}/products`, async (req, res, next) => {
+router.get(`/`, async (req, res) => {
     const productList = await Product.find();
 
     if (!productList) {
@@ -15,22 +15,41 @@ router.get(`/`, async (req, res, next) => {
 })
 
 
-// router.post(`${api}/products`, (req, res, next) => {
-router.post(`/`, (req, res, next) => {
-    const product = new Product({
+router.post(`/`, async (req, res) => {
+    const category = await Category.findById(req.body.category);
+    if(!category) {
+        return res.status(400).send('invalid category');
+    }
+
+    let product = new Product({
         name: req.body.name,
+        description: req.body.description,
+        richDescription: req.body.richDescription,
         image: req.body.image,
-        countInStock: req.body.countInStock
+        brand: req.body.brand,
+        price: req.body.price,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured: req.body.isFeatured
     })
 
-    product.save().then((createdProduct) => {
-        res.status(201).json(createdProduct)
-    }).catch((err) => {
-        res.status(500).json({
-            error: err,
-            success: false
-        })
-    })
+    product = await product.save();
+    if(!product) {
+        return res.status(500).send('the product cannot be created');
+    }
+    res.send(product);
+
+
+    // product.save().then((createdProduct) => {
+    //     res.status(201).json(createdProduct)
+    // }).catch((err) => {
+    //     res.status(500).json({
+    //         error: err,
+    //         success: false
+    //     })
+    // })
 })
 
 module.exports = router;
