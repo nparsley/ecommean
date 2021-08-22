31f693f8e27b1f7fac6953e3ae621e00f574d862
nparsley <nickparsley5@gmail.com>
2021-08-21 19:19:46 -0700

    userSchema add

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,12 +1,57 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: {
-        type: Number,
-        required: true
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    passwordHash: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    street: {
+        type: String,
+        default: ''
+    },
+    apartment: {
+        type: String,
+        default: ''
+    },
+    zip: {
+        type: String,
+        default: ''
+    },
+    city: {
+        type: String,
+        default: ''
+    },
+    country: {
+        type: String,
+        default: ''
     }
 })
 
+// virutals for _id
+userSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
+
+// enable when sending to front-end
+userSchema.set('toJSON', {
+    virtuals: true,
+});
+
 exports.User = mongoose.model('User', userSchema);
+exports.userSchema = userSchema;
